02700ba41ac747add252958fa2632777570fc40f
Zarif Jawad <zarif.jawad@quizrr.se>
2018-05-28 10:58:56 +0200

    Add a example CountDown Timer

--- football-warriors/src/App.js
+++ football-warriors/src/App.js
@@ -4,15 +4,13 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
+      return (
       <div className="App">
+
+
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Football warriors</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
